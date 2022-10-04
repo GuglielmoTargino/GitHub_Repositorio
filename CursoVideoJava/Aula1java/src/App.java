e1960ba977a2a9d43405d37da3264a9fa929fe8f
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2022-10-04 19:25:33 -0300

    Primeira Aula em Java

---
+++ CursoVideoJava/Aula1java/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    private static String resultado;
+    public static void main(String[] args) throws Exception {
+
+        int numero=10;
+        if (numero>=10) {
+            resultado="positivo";
+
+        }else{
+            resultado="negativo";
+
+        }
+        System.out.println("Hello, World!"+resultado);
+    }
+}
